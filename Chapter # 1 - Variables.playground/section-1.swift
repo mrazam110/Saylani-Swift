c495b630a67ea9491a0da3463d42e74a81e69389
Muhammad Raza Master <mrazam110@gmail.com>
2015-01-26 13:58:45 -0800

    Class1
    Class1

---
+++ Chapter # 1 - Variables.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+//Implicit Int
+var myInt = 10
+
+//Explicit Int
+var myExplicitInt : Int = 10
+
+//Implicit Double
+var myFloat = 10.5
+
+//Explicit Double
+var myExplicitFloat : Double = 10.5
+
+//Implicit String
+var myString = "My Name is Raza"
+
+//Explicit String
+var myExplicitString : String = "My Name is Raza"
+
+//Implicit Boolean
+var myBoolean = true
+
+//Explicit Boolean
+var myExplicitBoolean : Bool = false
---
+++ Chapter # 2 - Airthmetic Operator and String Interpolation.playground/section-1.swift
@@ -0,0 +1,34 @@
+// Playground - noun: a place where people can play
+
+var numberOne = 25
+var numberTwo = 10
+
+println("Results")
+
+var result = numberOne + numberTwo
+
+println("Addtion: \(result)")
+
+result = numberOne - numberTwo
+
+println("Subtraction: \(result)")
+
+result = numberOne * numberTwo
+
+println("Multiplication: \(result)")
+
+result = numberOne / numberTwo
+
+println("Division: \(result)")
+
+result = numberOne % numberTwo
+
+println("Modulus: \(result)")
+
+++result
+result++
+--result
+result--
+
+result+=2
+result-=2
